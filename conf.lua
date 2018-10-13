ed2c089d2139e932e2639e9d353b3faba7513fce
Matt Lee <thatismatt@gmail.com>
2018-10-13 22:52:29 +0100

    Add conf file.

---
+++ conf.lua
@@ -0,0 +1,7 @@
+love.conf = function(t)
+   t.title, t.identity = "Mugoar", "Mugoar"
+   t.modules.joystick, t.modules.physics = false, false
+   t.window.width, t.window.height = 1024, 768
+   t.window.vsync = false
+   t.version = "11.1"
+end
