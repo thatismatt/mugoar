ce4c15f31143ab23f69093443916c673b82cbba3
Matt Lee <thatismatt@gmail.com>
2018-11-21 22:40:35 +0000

    Upgrade to the latest version of fennel.

--- lib/fennelview.lua
+++ lib/fennelview.lua
@@ -19,18 +19,16 @@ local function _1_(...)
   return long
 end
 local long_control_char_esapes = _1_(...)
-local function _2_(str)
+local function escape(str)
   local str = str:gsub("\\", "\\\\")
   local str = str:gsub("(%c)%f[0-9]", long_control_char_esapes)
   return str:gsub("%c", short_control_char_escapes)
 end
-local escape = _2_
-local function _3_(k, len)
+local function sequence_key_3f(k, len)
   return ((type(k) == "number") and (1 <= k) and (k <= len) and (math.floor(k) == k))
 end
-local sequence_key_3f = _3_
 local type_order = {["function"] = 5, boolean = 2, number = 1, string = 3, table = 4, thread = 7, userdata = 6}
-local function _4_(a, b)
+local function sort_keys(a, b)
   local ta = type(a)
   local tb = type(b)
   if ((ta == tb) and (ta ~= "boolean") and ((ta == "string") or (ta == "number"))) then
@@ -49,38 +47,35 @@ local function _4_(a, b)
     end
   end
 end
-local sort_keys = _4_
-local function _5_(t)
+local function get_sequence_length(t)
   local len = 1
   for i in ipairs(t) do
     len = i
   end
   return len
 end
-local get_sequence_length = _5_
-local function _6_(t)
+local function get_nonsequential_keys(t)
   local keys = {}
   local sequence_length = get_sequence_length(t)
   for k in pairs(t) do
-    local function _7_()
+    local function _2_()
       if not sequence_key_3f(k, sequence_length) then
         return table.insert(keys, k)
       end
     end
-    _7_()
+    _2_()
   end
   table.sort(keys, sort_keys)
   return keys, sequence_length
 end
-local get_nonsequential_keys = _6_
-local function recur(t, appearances)
-  local function _7_()
+local function count_table_appearances(t, appearances)
+  local function _2_()
     if (type(t) == "table") then
       if not appearances[t] then
         appearances[t] = 1
         for k, v in pairs(t) do
-          recur(k, appearances)
-          recur(v, appearances)
+          count_table_appearances(k, appearances)
+          count_table_appearances(v, appearances)
         end
         return nil
       end
@@ -91,29 +86,25 @@ local function recur(t, appearances)
       end
     end
   end
-  _7_()
+  _2_()
   return appearances
 end
-local count_table_appearances = recur
 local put_value = nil
-local function _7_(self, ...)
+local function puts(self, ...)
   for _, v in ipairs({...}) do
     table.insert(self.buffer, v)
   end
   return nil
 end
-local puts = _7_
-local function _8_(self)
+local function tabify(self)
   return puts(self, "\n", self.indent:rep(self.level))
 end
-local tabify = _8_
-local function _9_(self, v)
+local function already_visited_3f(self, v)
   return (self.ids[v] ~= nil)
 end
-local already_visited_3f = _9_
-local function _10_(self, v)
+local function get_id(self, v)
   local id = self.ids[v]
-  local function _11_()
+  local function _2_()
     if not id then
       local tv = type(v)
       id = ((self["max-ids"][tv] or 0) + 1)
@@ -122,11 +113,10 @@ local function _10_(self, v)
       return nil
     end
   end
-  _11_()
+  _2_()
   return tostring(id)
 end
-local get_id = _10_
-local function _11_(self, t, length)
+local function put_sequential_table(self, t, length)
   puts(self, "[")
   self.level = (self.level + 1)
   for i = 1, length do
@@ -136,16 +126,14 @@ local function _11_(self, t, length)
   self.level = (self.level - 1)
   return puts(self, " ]")
 end
-local put_sequential_table = _11_
-local function _12_(self, k)
-  if ((type(k) == "string") and k:find("^[%w?\\^_`!#$%&*+-./@~:|<=>]+$")) then
+local function put_key(self, k)
+  if ((type(k) == "string") and k:find("^[-%w?\\^_`!#$%&*+./@~:|<=>]+$")) then
     return puts(self, ":", k)
   else
     return put_value(self, k)
   end
 end
-local put_key = _12_
-local function _13_(self, t)
+local function put_kv_table(self, t)
   puts(self, "{")
   self.level = (self.level + 1)
   for k, v in pairs(t) do
@@ -158,8 +146,7 @@ local function _13_(self, t)
   tabify(self)
   return puts(self, "}")
 end
-local put_kv_table = _13_
-local function _14_(self, t)
+local function put_table(self, t)
   if already_visited_3f(self, t) then
     return puts(self, "#<table ", get_id(self, t), ">")
   elseif (self.level >= self.depth) then
@@ -178,8 +165,7 @@ local function _14_(self, t)
     end
   end
 end
-local put_table = _14_
-local function _15_(self, v)
+local function _2_(self, v)
   local tv = type(v)
   if (tv == "string") then
     return puts(self, quote(escape(v)))
@@ -188,14 +174,14 @@ local function _15_(self, v)
   elseif (tv == "table") then
     return put_table(self, v)
   elseif "else" then
-    return puts(self, "#<", tv, " ", get_id(self, v), ">")
+    return puts(self, "#<", tostring(v), ">")
   end
 end
-put_value = _15_
-local function _16_(root, options)
+put_value = _2_
+local function fennelview(root, options)
   local options = (options or {})
   local inspector = {["max-ids"] = {}, appearances = count_table_appearances(root, {}), buffer = {}, depth = (options.depth or 128), ids = {}, indent = (options.indent or "  "), level = 0}
   put_value(inspector, root)
   return table.concat(inspector.buffer)
 end
-return _16_
+return fennelview
